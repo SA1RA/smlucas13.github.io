293d555b8298aa6c00462327b18c120c1d527909
Samuel Lucas <smlucas13@gmail.com>
2016-06-29 20:36:53 -0400

    Update play.js

--- project/js/play.js
+++ project/js/play.js
@@ -1,1 +1,23 @@
-//
+//game goes here VVV
+'use strict'
+var rome = {};
+rome.play = function () {};
+
+rome.prototype = {
+  init: function () {
+  //boot
+  console.log("%c~~~ Booting the st ~~~\n Compliments of Alucard and others",
+  "color:#fdf6e3; background:#07364");
+  },
+  preload: function () {
+  //game_screen
+  this.scale.pageAlignHorizontally = true;
+  this.scale.pageAlignVertically = true;
+  
+  //audio load
+  //sprites
+  this.load.image('background','project/field.png');
+  },
+}
+
+
