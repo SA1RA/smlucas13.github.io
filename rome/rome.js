f37e97b18c5cc6a09fe12dddefde2555424936c0
Samuel Lucas <smlucas13@gmail.com>
2016-07-27 19:49:09 -0400

    Update rome.js

--- rome/rome.js
+++ rome/rome.js
@@ -55,7 +55,7 @@ rome.Play.prototype = {
     }
     
     //animation
-    if (this.player.body.velocity.x = 400) {
+    if (this.player.body.velocity.x) {
       this.player.animations.add('blink');
       this.player.animations.play('blink',11,true);
     }
