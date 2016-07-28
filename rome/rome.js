c57759b1c35cb13abd8205e1b684d0f542747401
Samuel Lucas <smlucas13@gmail.com>
2016-07-27 20:19:33 -0400

    Update rome.js

--- rome/rome.js
+++ rome/rome.js
@@ -35,7 +35,7 @@ rome.Play.prototype = {
     this.player = this.add.sprite(400,750,'player');
     this.player.scale.set(3);
     this.player.anchor.setTo(0.5, 0.5);
-    this.player.add.animations.add('blink',6,true)
+    this.player.animations.add('blink',6,true)
     //this.player.animations.add('blink');
     //this.player.animations.play('blink',11,true);
     game.physics.arcade.enable(this.player);
@@ -51,8 +51,8 @@ rome.Play.prototype = {
     if (this.cursors.left.isDown) {
       this.player.animations.play('blink',6,true)
     }
-    if (this.player.body.velocity.x > 0) {
-      this.player.animations.play('blink',6,true);
+    if (this.player.body.velocity.x = 0) {
+      this.player.animations.play('blink',6,false);
       //flip
     }
     
