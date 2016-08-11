b71c3b872887b907774737f6f5fa2ff2035e520c
Samuel Lucas <smlucas13@gmail.com>
2016-08-10 20:04:00 -0400

    Update new_rome.js

--- rome/new_rome.js
+++ rome/new_rome.js
@@ -156,15 +156,15 @@ class PlayState {
     game.physics.arcade.collide(this.roman, this.skeleton);
     this.skeleton.body.velocity.x = 200;
     this.roman.body.velocity.x = 0;
-    if (this.skeleton.body.velocity.x = 0) {
-        this.skeleton.body.velocity.x = -200;
+    //if (this.skeleton.body.velocity.x = 0) {
+    //    this.skeleton.body.velocity.x = -200;
     //    if (this.facing != 'left') {
     //        this.skeleton.animations.play('left');
     //        this.facing = 'left';
     //    }
-    }
-    else if (this.skeleton.body.velocity.x = 0) {
-        this.skeleton.body.velocity.x = 200;
+    //}
+    //else if (this.skeleton.body.velocity.x = 0) {
+    //    this.skeleton.body.velocity.x = 200;
     //    if (this.facing != 'right'){
     //        this.skeleton.animations.play('right');
     //        this.facing = 'right';
